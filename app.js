e8ed2aa7403ed7642d8bcae8cf5641be048d8860
ParthKole <parthkole29@gmail.com>
2025-09-09 23:00:27 +0530

    basics of project

---
+++ app.js
@@ -0,0 +1,29 @@
+const express=require("express");
+const app=express();
+const mongoose=require("mongoose");
+
+
+async function main(){
+    mongoose.connect("mongodb://127.0.0.1:27017/wonderlust")
+}
+
+main()
+.then(()=>{
+    console.log("connected to DB...");
+})
+.catch((err)=>{
+    console.log(err);
+})
+
+
+
+
+app.listen(3000,(req,res)=>{
+    console.log("server is listening at port 3000...");
+})
+
+app.get("/",(req,res)=>{
+    res.send("on root page");
+})
+
+
